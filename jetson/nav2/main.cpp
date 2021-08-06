c3d12b87c4c823652149da58af51745b5ce5a030
zwgold <zwgold@users.noreply.github.com>
2021-08-05 20:46:42 -0400

    began rover changes for BT

--- jetson/nav2/main.cpp
+++ jetson/nav2/main.cpp
@@ -1,19 +1,129 @@
 #include <iostream>
 #include <lcm/lcm-cpp.hpp>
 #include "behaviortree_cpp_v3/bt_factory.h"
+#include "rover.hpp"
 #include "treeGenerator.hpp";
 using namespace rover_msgs;
 using namespace std;
 using namespace BT;
 
 
-// TODO LCMS
+// TODO, we need to create the variables and udpate functions in RoverStatus (preivously in StateMachine)
+// This class handles all incoming LCM messages for the autonomous
+// navigation of the rover.
+class LcmHandlers
+{
+public:
+    // Constructs an LcmHandler with the given status to work
+    // with.
+    LcmHandlers( Rover::RoverStatus* status )
+        : mStatus( status )
+    {}
 
+    // Sends the auton state lcm message to the state machine.
+    void autonState(
+        const lcm::ReceiveBuffer* recieveBuffer,
+        const string& channel,
+        const AutonState* autonState
+        )
+    {
+        mStatus->updateRoverStatus( *autonState );
+    }
+
+    // Sends the course lcm message to the state machine.
+    void course(
+        const lcm::ReceiveBuffer* recieveBuffer,
+        const string& channel,
+        const Course* course
+        )
+    {
+        mStatus->updateRoverStatus( *course );
+    }
+
+    // Sends the obstacle lcm message to the state machine.
+    void obstacle(
+        const lcm::ReceiveBuffer* receiveBuffer,
+        const string& channel,
+        const Obstacle* obstacle
+        )
+    {
+        mStatus->updateRoverStatus( *obstacle );
+    }
+
+    // Sends the odometry lcm message to the state machine.
+    void odometry(
+        const lcm::ReceiveBuffer* recieveBuffer,
+        const string& channel,
+        const Odometry* odometry
+        )
+    {
+        mStatus->updateRoverStatus( *odometry );
+    }
+
+    // Sends the target lcm message to the state machine.
+    void targetList(
+        const lcm::ReceiveBuffer* receiveBuffer,
+        const string& channel,
+        const TargetList* targetListIn
+        )
+    {
+        mStatus->updateRoverStatus( *targetListIn );
+    }
+
+    // Sends the radio lcm message to the state machine.
+    void radioSignalStrength(
+        const lcm::ReceiveBuffer* receiveBuffer,
+        const string& channel,
+        const RadioSignalStrength* signalIn
+        )
+    {
+        mStatus->updateRoverStatus( *signalIn );
+    }
+
+    // Updates Radio Repeater bool in state machine.
+    void repeaterDropComplete(
+        const lcm::ReceiveBuffer* receiveBuffer,
+        const string& channel,
+        const RepeaterDrop* completeIn
+        )
+    {
+        mStatus->updateRepeaterComplete( );
+    }
+
+private:
+    // The state machine to send the lcm messages to.
+    Rover::RoverStatus mStatus;
+};
 
 
 // Runs the autonomous navigation of the rover.
 int main()
 {
+    //TODO: get the rover object from a seperate function (uses rover config)
+    Rover* rover = new Rover();
+
+    //initialize lcms
+    lcm::LCM lcmObject;
+    if( !lcmObject.good() )
+    {
+        cerr << "Error: cannot create LCM\n";
+        return 1;
+    }
+
+    StateMachine roverStateMachine( lcmObject );
+    LcmHandlers lcmHandlers( &roverStateMachine );
+
+    lcmObject.subscribe( "/auton", &LcmHandlers::autonState, &lcmHandlers );
+    lcmObject.subscribe( "/course", &LcmHandlers::course, &lcmHandlers );
+    lcmObject.subscribe( "/obstacle", &LcmHandlers::obstacle, &lcmHandlers );
+    lcmObject.subscribe( "/odometry", &LcmHandlers::odometry, &lcmHandlers );
+    lcmObject.subscribe( "/radio", &LcmHandlers::radioSignalStrength, &lcmHandlers );
+    lcmObject.subscribe( "/rr_drop_complete", &LcmHandlers::repeaterDropComplete, &lcmHandlers );
+    lcmObject.subscribe( "/target_list", &LcmHandlers::targetList, &lcmHandlers );
+
+
+
+
     TreeGenerator treeGen;
 
     // reads the tree from xml file, registers the nodes (statically), then generates tree object
@@ -22,11 +132,10 @@ int main()
     // To "execute" a Tree you need to "tick" it.
     // The tick is propagated to the children based on the logic of the tree.
     // In this case, the entire sequence is executed, because all the children
-    // of the Sequence return SUCCESS.
+    // of the Sequence return SUCCESS.s
     BT::NodeStatus status = BT::NodeStatus::SUCCESS;
-    while (status == BT::NodeStatus::SUCCESS){
-        //lcm processing
-        //map processing
+    while (status == BT::NodeStatus::SUCCESS && (lcmObject.handle() == 0)){
+        //TODO: map processing
         status = tree.tickRoot();
     }
 
---
+++ jetson/nav2/rover.hpp
@@ -0,0 +1,140 @@
+#ifndef ROVER_HPP
+#define ROVER_HPP
+
+#include <lcm/lcm-cpp.hpp>
+#include <queue>
+
+#include "rover_msgs/AutonState.hpp"
+#include "rover_msgs/Bearing.hpp"
+#include "rover_msgs/Course.hpp"
+#include "rover_msgs/Odometry.hpp"
+#include "rover_msgs/RepeaterDrop.hpp"
+#include "rover_msgs/RadioSignalStrength.hpp"
+#include "rover_msgs/TargetList.hpp"
+#include "rover_msgs/Waypoint.hpp"
+#include "rapidjson/document.h"
+#include "pid.hpp"
+
+using namespace rover_msgs;
+using namespace std;
+
+
+// This class creates a Rover object which can perform operations that
+// the real rover can perform.
+class Rover
+{
+public:
+    // This class holds all the status informatin of the rover.
+    class RoverStatus
+    {
+    public:
+        RoverStatus();
+
+        RoverStatus(
+            Bearing bearingIn,
+            Course courseIn,
+            Odometry odometryIn,
+            Target targetIn,
+            Target target2In,
+            RadioSignalStrength signalIn
+            );
+
+        Destinations& destinations();
+
+        Odometry& odometry();
+
+        Target& target();
+
+        Target& target2();
+
+        RadioSignalStrength& radio();
+
+        RoverStatus& operator=( RoverStatus& newRoverStatus );
+
+    private:
+        // The rover's overall course.
+        Destinations mDestinations;
+
+        // The rover's current odometry information.
+        Odometry mOdometry;
+
+        // The rover's current target information from computer
+        // vision.
+        Target mTarget1;
+
+        Target mTarget2;
+
+        // the rover's current signal strength to the base station
+        RadioSignalStrength mSignal;
+
+    };
+
+    Rover( const rapidjson::Document& config, lcm::LCM& lcm_in );
+
+    DriveStatus drive( const Odometry& destination );
+
+    DriveStatus drive( const double distance, const double bearing, const bool target = false );
+
+    void drive(const int direction, const double bearing);
+
+    bool turn( Odometry& destination );
+
+    bool turn( double bearing );
+
+    void stop();
+
+    bool updateRover( RoverStatus newRoverStatus );
+
+    RoverStatus& roverStatus();
+
+    PidLoop& distancePid();
+
+    PidLoop& bearingPid();
+
+    const double longMeterInMinutes() const;
+
+    void updateRepeater( RadioSignalStrength& signal);
+
+    bool isTimeToDropRepeater();
+
+private:
+    /*************************************************************************/
+    /* Private Member Functions */
+    /*************************************************************************/
+    void publishJoystick( const double forwardBack, const double leftRight, const bool kill );
+
+    bool isEqual( const Odometry& odometry1, const Odometry& odometry2 ) const;
+
+    bool isEqual( const Target& target1, const Target& target2 ) const;
+
+    bool isTurningAroundObstacle( const NavState currentState ) const;
+
+    /*************************************************************************/
+    /* Private Member Variables */
+    /*************************************************************************/
+
+    // The rover's current status.
+    RoverStatus mRoverStatus;
+
+    // A reference to the configuration file.
+    const rapidjson::Document& mRoverConfig;
+
+    // A reference to the lcm object that will be used for
+    // communicating with the actual rover and the base station.
+    lcm::LCM& mLcmObject;
+
+    // The pid loop for driving.
+    PidLoop mDistancePid;
+
+    // The pid loop for turning.
+    PidLoop mBearingPid;
+
+    // If it is time to drop a radio repeater
+    bool mTimeToDropRepeater;
+
+    // The conversion factor from arcminutes to meters. This is based
+    // on the rover's current latitude.
+    double mLongMeterInMinutes;
+};
+
+#endif // ROVER_HPP
