7b62f2bc5e7e4324370680d210f84292db7c18da
Ankith Udupa <ankith.udupa@gmail.com>
2021-09-23 19:40:16 -0400

    adds base files for all the nodes to be written in

---
+++ jetson/nav2/nodes/gateNodes.hpp
@@ -0,0 +1,6 @@
+#include "behaviortree_cpp_v3/behavior_tree.h"
+#include "behaviortree_cpp_v3/bt_factory.h"
+
+namespace gateNodes {
+    void registerNodes(BT::BehaviorTreeFactory& factory);
+}
---
+++ jetson/nav2/nodes/searchNodes.hpp
@@ -0,0 +1,6 @@
+#include "behaviortree_cpp_v3/behavior_tree.h"
+#include "behaviortree_cpp_v3/bt_factory.h"
+
+namespace searchNodes {
+    void registerNodes(BT::BehaviorTreeFactory& factory);
+}
