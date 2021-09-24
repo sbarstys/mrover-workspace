3052f263b14ba3cae155f1dea5d6627cbcb480e3
John Trager <jtrager@umich.edu>
2021-09-23 20:55:47 -0400

    gate functions prototypes

---
+++ jetson/nav2/nodes/gateNodes.cpp
@@ -0,0 +1,10 @@
+#include "gateNodes.hpp"
+
+namespace VendingMachineNodes{
+
+
+
+
+
+
+}
--- jetson/nav2/nodes/gateNodes.hpp
+++ jetson/nav2/nodes/gateNodes.hpp
@@ -3,4 +3,30 @@
 
 namespace gateNodes {
     void registerNodes(BT::BehaviorTreeFactory& factory);
+
+    // Know First Gate Post Location?
+    BT::NodeStatus isFirstGatePostLocKnown();
+
+    // Generate Gate Traversal Path
+    BT::NodeStatus genGateTraversalPath();
+
+    // Gate Traversal Point?
+    BT::NodeStatus isGateTraversalPoint();
+
+    // No more Gate Traversal Waypoint left?
+    BT::NodeStatus noGateTraversalWaypoints();
+
+    // Traversal in wrong direction?
+    BT::NodeStatus didTraverseWrongDirection();
+
+    // Regenerate gate traversal waypoints
+    BT::NodeStatus regenGateTraversalWaypoints();
+
+    // More Gate traversal waypoints left?
+    BT::NodeStatus hasGateTraversalPoints();
+
+    // note gate post 1 found, gen search points
+    BT::NodeStatus genSecondPostSearchPattern();
+
+
 }
